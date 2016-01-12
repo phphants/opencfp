phphants/opencfp

commit 5f17d2d072a3e37b4f94a895cd037c156a193444

Set the end time in the configs so it is easier to specify the timezone to use

classes/Http/Controller/SignupController.php
@@ -19,7 +19,7 @@ class SignupController extends BaseController
             return $this->redirectTo('dashboard');
         }
 
-        if (strtotime($this->app->config('application.enddate') . ' 11:59 PM') < strtotime('now')) {
+        if (strtotime($this->app->config('application.enddate')) < strtotime('now')) {
             $this->app['session']->set('flash', array(
                 'type' => 'error',
                 'short' => 'Error',

classes/Http/Controller/TalkController.php
@@ -21,7 +21,7 @@ class TalkController extends BaseController
      */
     public function isCfpOpen($current_time)
     {
-        if ($current_time < strtotime($this->app->config('application.enddate') . ' 11:59 PM')) {
+        if ($current_time < strtotime($this->app->config('application.enddate'))) {
             return true;
         }
 

commit 87754e291ed79b67ac421c792788e33649b79bee

Also fix Signup Controller that regressed in 5f17d2d

classes/Http/Controller/SignupController.php
@@ -19,7 +19,7 @@ class SignupController extends BaseController
             return $this->redirectTo('dashboard');
         }
 
-        if (strtotime($this->app->config('application.enddate')) < strtotime('now')) {
+        if (strtotime($this->app->config('application.enddate') . ' 11:59 PM') < strtotime('now')) {
             $this->app['session']->set('flash', array(
                 'type' => 'error',
                 'short' => 'Error',